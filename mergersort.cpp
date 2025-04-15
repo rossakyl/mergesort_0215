742e5493c4dbd73afa61daaea4cff1ebd61035f0
rossakyl <rossakayla01@gmail.com>
2025-04-15 11:47:22 +0700

    Outputkan data setelah proses Merge Sort selesai

--- mergersort.cpp
+++ mergersort.cpp
@@ -90,5 +90,19 @@ for (int x = low; x <= high; x++)
 }
 
 
-
 void output ()
+{
+    cout << "\nData setelah diurutkan (Merge Sort): ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main ()
+{
+    input();
+    mergesort(0. n - 1);
+    output();
+}

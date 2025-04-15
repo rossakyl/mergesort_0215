b089dde0f2ace8c8448c683774cb61d7bc4cc088
rossakyl <rossakayla01@gmail.com>
2025-04-15 11:19:34 +0700

    Tambah fungsi mergeSort

--- mergersort.cpp
+++ mergersort.cpp
@@ -32,3 +32,39 @@ void input()
         cin >> arr[i];
     }
 }
+
+// create function mergeSort
+void mergeSort(int low, int high)
+{
+    if (low >= high) // step 1
+    {
+        return; // step 1.a
+    }
+
+    int mid = (low + high) / 2; // step 2
+
+    // step 3
+    // fungsi rekursi - memanggil diri sendiri
+    mergeSort(low, mid);    // step 3.a
+    mergeSort(mid + 1, high); // step 3.b
+
+    // step 4
+    int i = low;    // step 4.a
+    int j = mid + 1; // step 4.b
+    int k = low;    // step 4.c
+
+    while (i <= mid && j <= high) // step 4.d
+    {
+        if (arr[i] <= arr[j]) // step 4.d.i
+        {
+            B[k] = arr[i];
+            i++;
+        }
+        else
+        {
+            B[k] = arr[j];
+            j++;
+        }
+        k++; // step 4.d.ii
+    }
+}
